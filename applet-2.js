d31925ba50e7d5567941a56c53f0ec50f7bedd9f
jraldGITHUB <jraldybaco@gmail>
2024-10-07 23:08:01 +0800

    added Objects

--- applet-2.js
+++ applet-2.js
@@ -9,12 +9,12 @@ class leafLetMap{
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample for new corales BSIT student'
         }).addTo(this.map);
     }
-    }
+    
     addMarker(lat, long, message){
         const marker = L.marker([lat, long]).addTo(this.map)
         .bindPopup(message);
     }
-    
+}
     const Mymap = new leafLetMap('map', [8.359735, 124.869206], 18);
 
     
