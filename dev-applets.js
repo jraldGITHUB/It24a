042e21a112177cc39ff8de629331a4d1df13c1bb
jraldGITHUB <jraldybaco@gmail>
2024-10-07 22:23:35 +0800

    added aritmethic operators

--- dev-applets.js
+++ dev-applets.js
@@ -5,3 +5,10 @@ let text = "Im a BSIT student";
 let isActive = true;
 let array = [1, 2, 3, 4, 5];
 let person = { name: 'Jrald', age: 19 };
+
+// Arithmetic Operators
+let sum = number + number2;
+let difference = number - number2;
+let product = number * number2;
+let quotient = number / number2;
+let remainder = number % number2;
