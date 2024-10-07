48828a079019b81b9ebd28b5e5efd838172a45b1
jraldGITHUB <jraldybaco@gmail>
2024-10-07 22:26:09 +0800

    added functional programming

--- dev-applets.js
+++ dev-applets.js
@@ -13,6 +13,22 @@ let product = number * number2;
 let quotient = number / number2;
 let remainder = number % number2;
 
+
+// Prime number check function
+function isPrime(num) {
+    if (num <= 1) return false;
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) return false;
+    }
+    return true;
+}
+
+// Palindrome check function
+function isPalindrome(str) {
+    let reversed = str.split('').reverse().join('');
+    return str === reversed;
+}
+
 // Conditional Statements
 
 // if...else
@@ -52,4 +68,29 @@ let index = 0;
 while (index < array.length) {
     console.log('Array element at index', index, ':', array[index]);
     index++;
+}
+
+
+// Check if the number is even or odd
+console.log('\nOdd or Even Check:');
+if (number % 2 === 0) {
+    console.log(number, 'is an even number.');
+} else {
+    console.log(number, 'is an odd number.');
+}
+
+// Check if the number is a prime number
+console.log('\nPrime Number Check:');
+if (isPrime(number)) {
+    console.log(number, 'is a prime number.');
+} else {
+    console.log(number, 'is not a prime number.');
+}
+
+// Check if the text is a palindrome
+console.log('\nPalindrome Check:');
+if (isPalindrome(text.toLowerCase())) {
+    console.log('The text "' + text + '" is a palindrome.');
+} else {
+    console.log('The text "' + text + '" is not a palindrome.');
 }
