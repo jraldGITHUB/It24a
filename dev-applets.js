be4a0889a175389f41fdb9f079a9324591cb34de
jraldGITHUB <jraldybaco@gmail>
2024-10-07 22:24:40 +0800

    added conditional statements

--- dev-applets.js
+++ dev-applets.js
@@ -12,3 +12,28 @@ let difference = number - number2;
 let product = number * number2;
 let quotient = number / number2;
 let remainder = number % number2;
+
+// Conditional Statements
+
+// if...else
+console.log('\nConditional Statements (if...else):');
+if (number > 0) {
+    console.log('The number is positive.');
+} else if (number < 0) {
+    console.log('The number is negative.');
+} else {
+    console.log('The number is zero.');
+}
+
+// switch
+console.log('\nConditional Statements (switch):');
+switch (number) {
+    case 10:
+        console.log('The number is 10.');
+        break;
+    case 20:
+        console.log('The number is 20.');
+        break;
+    default:
+        console.log('The number is neither 10 nor 20.');
+}
