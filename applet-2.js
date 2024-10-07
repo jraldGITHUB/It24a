a84ed5f033ce4af7257ae81101471efa5d2d3d9f
jraldGITHUB <jraldybaco@gmail>
2024-10-07 22:56:48 +0800

    added  initTilLayer()

--- applet-2.js
+++ applet-2.js
@@ -1,5 +1,12 @@
 class leafLetMap{
     constructor(containerId, center, zoom){
         this.map = L.map(containerId).setView(center, zoom);
+        this.initTileLayer();
     }
-}
+    initTileLayer() {
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | Sample for new corales BSIT student'
+        }).addTo(this.map);
+    }
+    }
