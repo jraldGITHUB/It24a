294066fe33e56ce96a642a62ddf2455baa6b081d
jraldGITHUB <jraldybaco@gmail>
2024-10-08 16:54:31 +0800

    added upadateMarkerPopup() and loadMarkersfromJson()

--- applet-3.5.js
+++ applet-3.5.js
@@ -49,4 +49,20 @@ addMarker(lat, long, message){
     this.updateMarkerPopup(marker, message);
     this.markers.push(marker);
 }
+
+updateMarkerPopup(marker, message) {
+    const count = this.markerCounts[message];
+    marker.bindPopup(`${message}<br>Attendance logs: ${count}`).openPopup();
+}
+
+loadMarkersFromJson(url) {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        data.forEach(marker => {
+            this.addMarker(marker.latitude, marker.longitude, marker.message);
+        });
+    })
+    .catch(error => console.error("Error Loading servers:", error));
+}
 }   
