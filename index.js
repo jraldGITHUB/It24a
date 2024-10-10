0d619a5d24a95a9356e4923be40b0817f0699e2d
jraldGITHUB <jraldybaco@gmail>
2024-10-10 22:04:53 +0800

    added init()

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ class cardSearch {
         this.searchInput = document.getElementById(searchInputId);
         this.cardContainer = document.querySelector(`.${cardContainerClass}`);
         this.cards = this.cardContainer.querySelectorAll('.card');
+        this.init();
+    }	    
+
+     init() {
+        this.searchInput.addEventListener('input', () => {
+            this.filterCards(this.searchInput.value);
+        });
     }
+
 }
