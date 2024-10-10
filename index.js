eaa9d1bcb532a23c40255c861b3bc63ac85c61c6
jraldGITHUB <jraldybaco@gmail>
2024-10-10 22:59:29 +0800

    added DOMContentLoaded() and add final changers

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-class cardSearch {
+class CardSearch {
     constructor(searchInputId, cardContainerClass) {
         this.searchInput = document.getElementById(searchInputId);
         this.cardContainer = document.querySelector(`.${cardContainerClass}`);
-        this.cards = this.cardContainer.querySelectorAll('.card');
+        this.cards = this.cardContainer.querySelectorAll('.col-md-4');
         this.init();
-    }	    
+    }
 
-     init() {
+    init() {
         this.searchInput.addEventListener('input', () => {
             this.filterCards(this.searchInput.value);
         });
     }
+
     filterCards(query) {
         const searchTerm = query.toLowerCase();
 
-        this.cards.forEach(card => {
+        this.cards.forEach(cardContainer => {
+            const card = cardContainer.querySelector('.card');
             const title = card.querySelector('.card-title').textContent.toLowerCase();
             const text = card.querySelector('.card-text').textContent.toLowerCase();
 
             if (title.includes(searchTerm) || text.includes(searchTerm)) {
-                card.closest('.col-sm-4').style.display = '';
+                cardContainer.style.display = ''; 
             } else {
-                card.closest('.col-sm-4').style.display = 'none';
+                cardContainer.style.display = 'none'; 
             }
         });
     }
-
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    new CardSearch('Cardsearch', 'container')
+});
